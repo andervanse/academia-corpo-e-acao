5793c897cc7b7dd171781ce949a0e56c2c6616e3
Anderson Davanse <anderson.davanse@tmf-group.com>
2019-01-22 16:26:32 -0200

    Basic structure

--- academia-corpo-e-acao/src/app/app.module.ts
+++ academia-corpo-e-acao/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { LoginComponent } from './components/login/login.component';
+
+const appRoutes :Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', redirectTo: '' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    ContatoComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
